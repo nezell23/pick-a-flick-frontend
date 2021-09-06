8782e270a597e72f17ded64c76a37f70aa62d59f
Nichole Ezell <nezell8487@gmail.com>
2021-09-06 16:49:19 -0400

    created movies, tags, & users services w/CRUD for movies & tags

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http'
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
@@ -34,7 +36,8 @@ import { EditTagsComponent } from './components/edit-tags/edit-tags.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
