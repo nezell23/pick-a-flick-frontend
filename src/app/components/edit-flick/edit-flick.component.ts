fffc32fe82ade68e7a31a0316bdb108d9c53e248
Nichole Ezell <nezell8487@gmail.com>
2021-09-08 22:42:08 -0400

    added code to editFlickComponent ts file, will complete html file tomorrow

--- src/app/components/edit-flick/edit-flick.component.ts
+++ src/app/components/edit-flick/edit-flick.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Movie } from 'src/app/models/movie';
+import { MoviesService } from 'src/app/services/movies.service';
 
 @Component({
   selector: 'app-edit-flick',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditFlickComponent implements OnInit {
 
-  constructor() { }
+  editMovie: Movie;
+  currentId: number;
+
+  constructor(private moviesService: MoviesService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    // get the id of the movie being updated
+    this.currentId = parseInt(this.route.snapshot.paramMap.get("movieId"));
+
+    // display movie info in fields to edit
+    this.moviesService.getMovieById(this.currentId).subscribe(response => {
+      this.editMovie = response;
+      console.log(response);
+    });
+
+  }
+
+  // take movie info from form & update in DB when save button clicked
+  saveEditMovie() {
+    this.moviesService.editMovie(this.currentId, this.editMovie).subscribe(response => {
+      this.router.navigate([`movies/${this.currentId}`]) // need to put route here for View Flick page...not sure if this is correct notation...test!
+    })
   }
 
 }
