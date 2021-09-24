9ad9ef97c6f6dc5e877e94c310f1356f01d4d1ad
Nichole Ezell <nezell8487@gmail.com>
2021-09-24 12:58:15 -0400

    updated routes in services to match backend

--- global.ts
+++
@@ -1,3 +0,0 @@
-export const GlobalVariable = Object.freeze ({
-    BASE_API_URL: 'http://localhost:3000'  // this is url for mock DB - update to backend later
-});
--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { GlobalVariable } from 'global';
+import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
 import { Movie } from '../models/movie';
 
@@ -9,39 +9,37 @@ import { Movie } from '../models/movie';
 })
 export class MoviesService {
 
-  // GlobalVariable.BASE_API_URL = http://localhost:3000
-  // this is url for mock DB - update to backend later
-  apiServerUrl: string = GlobalVariable.BASE_API_URL;
+  // environment.apiBaseUrlMovies: 'http://localhost:8080/api'
+  apiServerUrl: string = environment.apiBaseUrlMovies;
 
   constructor(private http: HttpClient) { }
 
   // get all movies READ
-  getMovies(tag?: string): Observable<Movie[]> {
-    return this.http.get<Movie[]>(`${this.apiServerUrl}/movies?tag=${tag}`);
+  getMovies(): Observable<Movie[]> {
+    return this.http.get<Movie[]>(`${this.apiServerUrl}/movies/all`);
   }
-
 
   // get movie by id READ
   // need to provide movieId
   getMovieById(movieId: number): Observable<Movie> {
-    return this.http.get<Movie>(`${this.apiServerUrl}/movies/${movieId}`);
+    return this.http.get<Movie>(`${this.apiServerUrl}/movies/find/${movieId}`);
   }
 
   // edit movie UPDATE
   // need to provide editId and updatedMovie info
   editMovie(editId: number, updatedMovie: Movie): Observable<Movie> {
-    return this.http.put<Movie>(`${this.apiServerUrl}/movies/${editId}`, updatedMovie);
+    return this.http.put<Movie>(`${this.apiServerUrl}/movies/update/${editId}`, updatedMovie);
   }
 
   // add movie CREATE
   // need to provide newMovie info
   addMovie(newMovie: Movie): Observable<Movie> {
-    return this.http.post<Movie>(`${this.apiServerUrl}/movies`, newMovie);
+    return this.http.post<Movie>(`${this.apiServerUrl}/movies/add`, newMovie);
   }
 
   // delete movie DELETE
   // need to provide deleteId
   deleteMovie(deleteId: number): Observable<any> {
-    return this.http.delete<any>(`${this.apiServerUrl}/movies/${deleteId}`);
+    return this.http.delete<any>(`${this.apiServerUrl}/movies/delete/${deleteId}`);
   }
 }
